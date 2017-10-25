f820409b9939cb94c2d85a4def4a5039a23d4e9f
Arun Chennadi <aruntejbombay@gmail.com>
2017-10-25 14:12:06 -0700

    Twitter questions

---
+++ src/com/arun/companies/twitter/IPAddressvalidation.java
@@ -0,0 +1,66 @@
+/*
+ * Complete the function below.
+ */
+
+static String[] checkIP(String[] ip) {
+        String[] results = new String[ip.length];
+
+        for(int i = 0; i < ip.length; i++){
+        results[i] = validIPAddress(ip[i]);
+        }
+        return results;
+        }
+
+public static String validIPAddress(String IP) {
+
+        if(IP == null || IP.equalsIgnoreCase(""))
+        return "Neither";
+
+        String[] v4 = IP.split("\\.", -1);
+        String[] v6 = IP.split(":", -1);
+
+        if(v4.length != 4 && v6.length != 8)
+        return "Neither";
+
+        if(v4.length == 4){
+        for(String s : v4){
+        if((s.startsWith("0") && s.length() > 1) || s.startsWith("-"))
+        return "Neither";
+        boolean isNum = true;
+        int no = 0;
+
+        try{
+        no = Integer.parseInt(s);
+        }catch(Exception e){
+        isNum = false;
+        }
+        if(!isNum || no < 0 || no > 255){
+        return "Neither";
+        }
+        }
+
+        return "IPv4";
+        }
+
+        if(v6.length == 8){
+        for(String s : v6){
+        if(s.length() > 4 || s.startsWith("-")){
+        return "Neither";
+        }
+        boolean isNum = true;
+        long no = 0;
+
+        try{
+        no = Long.parseLong(s,16);
+        }catch(Exception e){
+        isNum = false;
+        }
+        if(!isNum || no < 0){
+        return "Neither";
+        }
+        }
+        return "IPv6";
+        }
+        return "Neither";
+        }
+
--- src/com/arun/companies/twitter/TimeSeriesDataAggregation.java
+++ src/com/arun/companies/twitter/TimeSeriesDataAggregation.java
@@ -1,56 +1,70 @@
-import fileinput
-from collections import OrderedDict
-import collections
-givenstr=""
-        for line in fileinput.input():
-        givenstr=givenstr+line
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> details = new ArrayList<String>();
+        while (input.hasNext()) {
+            details.add(input.nextLine());
+            //System.out.println(input.nextLine());
+        }
+
+        ArrayList<String> output = aggregate(details);
+        for(String det : output){
+            System.out.println(det);
+        }
+    }
+
+    public static ArrayList<String> aggregate(ArrayList<String> details){
+        String testDateString = "02/04/2014";
+        DateFormat df = new SimpleDateFormat("yyyy-MM");
+        TreeMap<Date,TreeMap<String,Integer>> data = new TreeMap<Date,TreeMap<String,Integer>>();
 
 
+        try{
+            Date start = df.parse(details.get(0).split(",")[0]);
+            Date end = df.parse(details.get(0).split(",")[1]);
+            for(int i = 2; i < details.size();i++){
+                String[] lineData = details.get(i).split(",");
+                Date time = df.parse(lineData[0]);
+                if(time.compareTo(start) < 0 || time.compareTo(end) >= 0){
+                    continue;
+                }
+                if(!data.containsKey(time)){
+                    data.put(time, new TreeMap<String,Integer>());
+                }
+                TreeMap<String,Integer> data2 = data.get(time);
+                if(!data2.containsKey(lineData[1])){
+                    data2.put(lineData[1], 0);
+                }
+                data2.put(lineData[1], data2.get(lineData[1]) + Integer.parseInt(lineData[2].trim()));
+                //System.out.println(new SimpleDateFormat("yyyy-MM").format(time));
+            }
+            //System.out.println(data.toString());
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        ArrayList<String> ret = new ArrayList<String>();
+        Set<Date> keys = data.keySet();
+        for(Date key: keys){
+            TreeMap<String, Integer> hits = data.get(key);
+            StringBuilder temp = new StringBuilder();
+            temp.append(new SimpleDateFormat("yyyy-MM").format(key));
+            Set<String> k2 = hits.keySet();
+            for(String k : k2){
+                temp.append(","+k+", "+hits.get(k));
+            }
 
+            ret.add(temp.toString());
+        }
+        Collections.reverse(ret);
 
-        def fib_dyn(givenstr):
-        startyear = givenstr[0].split(",")[0].split("-")[0]
-        startmonth = givenstr[0].split(",")[0].split("-")[1]
-        endyear = givenstr[0].split(",")[1].split("-")[0].replace(" ", "")
-        endmonth = givenstr[0].split(",")[1].split("-")[1]
-        capture={}
+        return ret;
+    }
 
-        givenstr=givenstr[2:len(givenstr)-1]
-
-        for line in range( len(givenstr) ):
-        year = givenstr[line].split("-")[0]
-        month = givenstr[line].split("-")[1]
-
-        if(len(givenstr[line].split(","))>2):
-        data=givenstr[line].split(",")
-
-        if (int(year+month) >= int(startyear+startmonth) and int(year+month) < int(endyear+endmonth)):
-        datatype=data[1].replace(" ","")
-        dataofdatatype=data[2].replace(" ","")
-
-
-        key=str(year+"-"+month)
-
-        if key not in capture:
-        capture[key] = {}
-        capture[str(key)][datatype] = {}
-        capture[str(key)][datatype] = dataofdatatype
-        else:
-
-        if datatype in capture[str(key)]:
-        capture[str(key)][datatype]=str(int(capture[str(key)][datatype])+int(dataofdatatype))
-        else:
-        capture[str(key)][datatype] = {}
-        capture[str(key)][datatype]=dataofdatatype
-        if(int(dataofdatatype)<=0):
-        capture[str(key)].pop(datatype)
-
-        capture = collections.OrderedDict(sorted(capture.items(),reverse=True))
-        for keyH,valueH in capture.items():
-        capture[keyH]=OrderedDict(sorted(capture[keyH].items(), key=lambda t: t[0]))
-        print(keyH,end="")
-        for key, value in capture[keyH].items():
-        print(", "+key+", "+value,end="")
-        print("")
-        givenstr = givenstr.split("\n")
-        fib_dyn (givenstr)
+}
